c0222d5f9b5d0b12a9a2be31d56f895237d8e3a0
HessahAlK <hessah.alkhudhair@gmail.com>
2018-10-07 15:49:15 +0300

    task submission

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 
 def welcome(request):
-    return render(request, 'index.html', {'msg':'Hello World!'})
+	return render(request, 'index.html', {'msg':'Hello World!'})
 
 def restaurant_list(request):
-
-    context = {
-
-    }
-    return render(request, 'list.html', context)
+	restaurants=[
+		{
+			'name':'Bella',
+			'type': 'Italian'
+		},
+		{
+			'name':'Zara',
+			'type':'Indian'
+		},
+		{
+			'name':'Bazza',
+			'type':'Kuwaiti'
+		},
+	]
+	context = {
+		'my_list': restaurants,
+	    }
+	return render(request, 'list.html', context)
 
 
 def restaurant_detail(request):
+	restaurantsdet={
+	'name':'Bella', 'type':'Italian'
+	}
 
-    context = {
+	context = {'my_object': restaurantsdet,
 
-    }
-    return render(request, 'detail.html', context)
+	}
+	return render(request, 'detail.html', context)
